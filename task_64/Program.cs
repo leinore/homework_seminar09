9d29b033170acf9508fef54395b8e4a87e4e9211
leinore <116540768+leinore@users.noreply.github.com>
2022-12-14 20:40:17 +0300

    add task_64

---
+++ task_64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+void Numbers(int num)
+{
+    if (num == 0)
+    {
+        return;
+    }
+    Console.Write($"{num}");
+    Numbers(num-1);
+}
+Console.Write("Введите число: ");
+int nums = int.Parse(Console.ReadLine());
+Numbers(nums);
